c0225f1c74ca0a3db1137a1aef490b12aec23f16
Amityadav-79 <79058885+Amityadav-79@users.noreply.github.com>
2021-02-15 00:54:55 +0530

    Add files via upload

---
+++ first.php
@@ -0,0 +1,26 @@
+<html>
+<head><h1>First PHP With my personal server </h1>
+</head>
+<body>
+    
+	<?php
+	
+	    
+		
+		
+	  print "running my first php program with xamp sever.{1} SOFTWARE ENGINEER {2} AI ENGINEER {3} ML ENGINEER The CGI Process
+The basic principle of Common Gatng data back to the browser. HTTP and HTML specwidely in use today (although technically required for HTTP1.ecification,
+ text data that is not tagged can be assumed to be in the default character set ISO-8859-1 (US-ASCII). The server correlates this cidentifier (CCSID) 819.
+Writing high availability CGI programs
+High availability CGI programs use APIs to preserve  in a clustered environment, even after a failure or switchover of the HTTP Server or IBM i server.
+Writing persistent CGI programs
+Persistent CGI is an  allows files to be left open, the state to be maintained, and long running database transactions to be committed or rolled-back based on end-user input."
+	  
+	
+	  
+	  
+	  
+	?>
+	
+</body>
+</html>
